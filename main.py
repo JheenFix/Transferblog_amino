f103908886bce81958c2673fe117132edf9ccc30
JheenFix <167264358+JheenFix@users.noreply.github.com>
2024-04-18 13:21:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import os
-os.system("pip uninstall Aminofix")
-os.system("pip uninstall BotAmino")
-os.system("pip install tqdm")
-os.system("pip install https://github.com/ThePhoenix78/BotAmino/archive/master.zip")
-os.system("pip install colorama")
-os.system("pip install colored")
-os.system("pip install requests")
-os.system("pip install pystyle")
+try:
+    import Aminofix
+except ImportError:
+    os.system("pip uninstall Aminofix")
+    os.system("pip install tqdm")
+try:
+    import BotAmino
+except ImportError:
+    os.system("pip uninstall BotAmino")
+    os.system("pip install https://github.com/ThePhoenix78/BotAmino/archive/master.zip")
+try:
+    import colorama
+except ImportError:
+    os.system("pip install colorama")
+try:
+    import colored
+except ImportError:
+    os.system("pip install colored")
+try:
+    import requests
+except ImportError:
+    os.system("pip install requests")
+try:
+    import pystyle
+except ImportError:
+    os.system("pip install pystyle")
+    
 import marshal
-
 with open('blog.py', 'rb') as file:
     codigo_ofuscado = file.read()
 
